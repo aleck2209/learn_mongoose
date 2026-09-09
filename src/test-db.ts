cc18a895026d4ab9d1c3ff7567889dc494bc2b1e
aleck2209 <aleckyann09@gmail.com>
2026-09-09 12:08:48 +0100

    feat: rechercher un utilisateur à l'aide de findOne et retourner tout le contenu d'un document à l'aide de find

--- src/test-db.ts
+++ src/test-db.ts
@@ -3,12 +3,8 @@ import { User } from "./modules/user/user.model.ts";
 
 await connectDatabase();
 
-const user = new User({
-    name: "Virgile",
-    email: "aleckyann@09gmail.com",
-    age: 25
+const user = await User.findOne({
+    email: 'aleckyann@09gmail.com'
 });
-
-await user.save();
 
 console.log(user);
